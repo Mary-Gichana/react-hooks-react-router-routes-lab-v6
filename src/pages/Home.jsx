f83cb274d545ab63e64cc2e8e4e3b6a42b133b00
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-17 21:20:32 +0300

    complete lab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,26 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
-function Home() {
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>Home Page</h1>
+      <div>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
   );
 };
 
